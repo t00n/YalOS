61eb60ffb9fcca4610b000256dc619fb80620fbe
t00n <kelkununtel@hotmail.com>
2015-06-22 17:50:25 +0200

    [add] get free page frame

--- memory.c
+++ memory.c
@@ -11,7 +11,7 @@ void mem_init()
 	int i = 0, address = 0;
 	unsigned int cr0;
 	// create page directory after the end of kernel
-	unsigned int page_aligned_end = (endkernel & 0xFFFFF000) + 0x1000;
+	unsigned int page_aligned_end = (endkernel & PAGE_ADDR) + 0x1000;
 	page_directory = (unsigned int*)page_aligned_end;
 	for(i = 0; i < 1024; i++)
 	{
@@ -48,38 +48,47 @@ void map_page(unsigned int virtual_addr, unsigned int physical_addr, unsigned in
 	page = (virtual_addr >> 12) & 0x3FF; // 10 next bits
 
 	new_page_table = getPageTable(pdir);
-	new_page_table[page] = (physical_addr & 0xFFFFF000) | PAGE_PRESENT | flags;
+	new_page_table[page] = (physical_addr & PAGE_ADDR) | PAGE_PRESENT | flags;
 	if ((page_directory[pdir] & PAGE_PRESENT) == 0) // if page dir not present, create page dir
 	{
 		page_directory[pdir] = (unsigned int)new_page_table | PAGE_PRESENT | flags;
 	}
 }
 
-// simple page fault handler that maps virtual memory identically
+// simple page fault handler that maps to first free page frame
 void page_fault_handler(struct regs * r)
 {
 	unsigned int cr2;
+	unsigned int frame = get_free_page_frame();
 	// get fault address
 	asm volatile("mov %%cr2, %0": "=b"(cr2));
 	if ((r->err_code & 1) == 0) // page not present in memory
 	{
 		if ((r->err_code & 2) == 0) // read access
 		{
-			map_page(cr2, cr2, PAGE_NOTHING);
+			map_page(cr2, frame, PAGE_NOTHING);
 		}
 		else // write access
 		{
-			map_page(cr2, cr2, PAGE_RW);
+			map_page(cr2, frame, PAGE_RW);
 		}
 	}
 	else // page present -> protection fault
 	{
 		vga_puts("protection fault");
-
 	}
 }
 
 unsigned int get_free_page_frame()
 {
-
+	for (int i = 0; i < 1024; ++i) {
+		unsigned int * page_dir = getPageTable(i);
+		for (int j = 0; j < 1024; ++j) {
+			unsigned int page = *(page_dir + j);
+			if ( !(page & PAGE_PRESENT)) {
+				return (page & PAGE_ADDR) >> 10;
+			}
+		}
+	}
+	// if we arrive here there is no free page frame
 }
